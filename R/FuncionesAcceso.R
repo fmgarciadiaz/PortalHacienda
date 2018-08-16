10088fed7ff22b305eb9a4fead6d2d5954248531
Fernando Garcia Diaz <fmgarciadiaz78@gmail.com>
2018-08-16 12:50:15 -0300

    v 0.5.1

--- R/FuncionesAcceso.R
+++ R/FuncionesAcceso.R
@@ -57,7 +57,7 @@ freq <- function(x) {
 #' # Cargar serie mensual de TCN, transformada en anual y en variaciones
 #' TCN <- Get("174.1_T_DE_CATES_0_0_32", start_date = 1999, collapse = "year", collapse_aggregation = "avg", representation_mode = "percent_change")
 Get <- function(series, start_date = NULL, end_date = NULL, representation_mode = NULL,
-                collapse = NULL, collapse_aggregation = NULL, limit = 1000, timeout = 0.2) {
+                collapse = NULL, collapse_aggregation = NULL, limit = 1000, timeout = 5) {
   url_base <- "http://apis.datos.gob.ar/series/api/series?"                                      # Cambiar URL base si cambia en la WEB
   GETSAFE <- purrr::safely(httr::GET)   # enmarco en purrr para manejo de errores
   suppressMessages(serie <- GETSAFE(url = url_base, query = list(ids = series,
@@ -69,7 +69,9 @@ Get <- function(series, start_date = NULL, end_date = NULL, representation_mode 
                                                                       format = "csv",
                                                                       limit = limit),
                                                                       httr::timeout(timeout)))
-  if (is.null(serie$result)) stop("Error general, por favor verificar conexión.")
+  if (is.null(serie$result)) {
+    stop("Error general o timeout, por favor verificar conexión.")
+    }
   suppressMessages(serie <- httr::content(serie$result , encoding = "UTF-8"))
   if ("errors" %in% names(serie)) stop("Error en la carga > " %+% serie$errors[[1]][[1]])
   Nombres <-  Listado %>% dplyr::filter(grepl(gsub("\\,", "\\|", series), serie_id)) %>%
